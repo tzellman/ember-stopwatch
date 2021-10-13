64356dcd58439af80da93c940617257771cd21b5
Tom Zellman <tzellman@gmail.com>
2021-10-13 07:05:26 -0400

    chore(deps): update ember-cli to 3.28.1
    also, update ember-render-modifiers and tailwindcss to latest

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,17 +23,16 @@ module.exports = {
         // node files
         {
             files: [
-                '.eslintrc.js',
-                '.template-lintrc.js',
-                'commitlint.config.js',
-                'ember-cli-build.js',
-                'index.js',
-                'testem.js',
-                'blueprints/*/index.js',
-                'config/**/*.js',
-                'tests/dummy/config/**/*.js'
+                './.eslintrc.js',
+                './.template-lintrc.js',
+                './commitlint.config.js',
+                './ember-cli-build.js',
+                './index.js',
+                './testem.js',
+                './blueprints/*/index.js',
+                './config/**/*.js',
+                './tests/dummy/config/**/*.js'
             ],
-            excludedFiles: ['addon/**', 'addon-test-support/**', 'app/**', 'tests/dummy/app/**'],
             parserOptions: {
                 sourceType: 'script'
             },
